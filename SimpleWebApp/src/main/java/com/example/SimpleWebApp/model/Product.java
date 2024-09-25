953cb7d9e800248abc173831fdee82476137ff06
Phu Kyaw Win <94231078+phukyawwin@users.noreply.github.com>
2024-09-25 17:39:01 +0800

    SpringBoot

---
+++ SimpleWebApp/src/main/java/com/example/SimpleWebApp/model/Product.java
@@ -0,0 +1,16 @@
+package com.example.SimpleWebApp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Component
+public class Product {
+    private int prodId;
+    private String prodName;
+    private int price;
+}
