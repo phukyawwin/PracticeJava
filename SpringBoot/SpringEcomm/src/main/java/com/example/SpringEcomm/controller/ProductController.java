30b9c982a8aaa710e35fdf01f63492f211bc1048
Phu Kyaw Win <94231078+phukyawwin@users.noreply.github.com>
2024-09-27 16:09:53 +0800

    add fetch image

--- SpringBoot/SpringEcomm/src/main/java/com/example/SpringEcomm/controller/ProductController.java
+++ SpringBoot/SpringEcomm/src/main/java/com/example/SpringEcomm/controller/ProductController.java
@@ -4,6 +4,7 @@ import com.example.SpringEcomm.model.Product;
 import com.example.SpringEcomm.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -46,4 +47,13 @@ public class ProductController {
 
         }
     }
+    @GetMapping("/product/{productId}/image")
+    public ResponseEntity<byte[]> getImageByProductId(@PathVariable int productId) {
+        Product product = service.getProduct(productId);
+        byte[] imageFile = product.getImageData();
+
+        return ResponseEntity.ok().contentType(MediaType.valueOf(product.getImageType())).body(imageFile);
+
+    }
+
 }
