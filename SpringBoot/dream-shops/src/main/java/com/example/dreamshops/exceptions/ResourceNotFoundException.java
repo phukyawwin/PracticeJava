4ed9bd10bf46baecea8303f0cf14d393ec6da21f
Phu Kyaw Win <94231078+phukyawwin@users.noreply.github.com>
2024-09-28 07:41:13 +0800

    add product service

---
+++ SpringBoot/dream-shops/src/main/java/com/example/dreamshops/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.dreamshops.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+
+    }
+}
