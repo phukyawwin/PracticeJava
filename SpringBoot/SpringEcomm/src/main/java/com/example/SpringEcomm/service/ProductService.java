799951e84d19e08a3ffa12965687323e236f5353
Phu Kyaw Win <94231078+phukyawwin@users.noreply.github.com>
2024-09-27 15:56:37 +0800

    add product image

--- SpringBoot/SpringEcomm/src/main/java/com/example/SpringEcomm/service/ProductService.java
+++ SpringBoot/SpringEcomm/src/main/java/com/example/SpringEcomm/service/ProductService.java
@@ -4,7 +4,9 @@ import com.example.SpringEcomm.model.Product;
 import com.example.SpringEcomm.repo.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 @Service
 public class ProductService {
@@ -18,5 +20,13 @@ public class ProductService {
 
     public Product getProduct(int id) {
         return repo.findById(id).orElse(null);
+    }
+
+    public Product addProduct(Product product, MultipartFile imageFile) throws IOException {
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        product.setImageData(imageFile.getBytes());
+
+        return repo.save(product);
     }
 }
